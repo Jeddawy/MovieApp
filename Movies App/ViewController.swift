681c69f433c99fe6420184003c96342d3222e06f
Ibrahim Ahmed El-geddawy <ielgeddawy@gmail.com>
2019-08-20 19:58:49 +0200

    Initial Commit

---
+++ Movies App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies App
+//
+//  Created by Ibrahim El-geddawy on 8/20/19.
+//  Copyright © 2019 Ibrahim El-geddawy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
